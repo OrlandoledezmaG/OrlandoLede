d3b07149f99b04db146fe0d9a653bb36c088f89f
OrlandoledezmaG <batomic33@gmail.com>
2022-05-25 08:05:36 -0500

    added the css and html

---
+++ script.js
@@ -0,0 +1,13 @@
+function aboutSection() {
+    document.getElementById('aboutTitle').scrollIntoView()
+    console.log("hi")
+}
+
+aboutSection()
+
+function contactSection() {
+    document.getElementById('contactTitle').scrollIntoView()
+    console.log("hi")
+}
+
+contactSection()
